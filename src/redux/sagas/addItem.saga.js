f6203e669df33bfb172bdd97b48e7a719a8d6208
johnturner4004 <69251742+johnturner4004@users.noreply.github.com>
2021-05-19 16:22:54 -0400

    Update addItem.saga.js

--- src/redux/sagas/addItem.saga.js
+++ src/redux/sagas/addItem.saga.js
@@ -3,14 +3,7 @@ import { put, takeEvery } from "redux-saga/effects";
 
 
 // Gets the item from the database
-function* fetchAddItem (action) {
-    try {
-        const response = yield axios.get(`/api/shelf/${action.payload.id}`);
-        yield put({type: 'SET_ADD_ITEM', payload: response.data});
-    } catch (error) {
-        console.log('Failed In fetch item ', error);
-    }
-}
+
 
 // Adds new item entry
 
@@ -24,7 +17,6 @@ function* AddNewItem(action) {
 }
 
 function* addItemSaga() {
-    yield takeEvery('FETCH_ADD_ITEM', fetchAddItem);
     yield takeEvery('ADDING_NEW_ITEM', AddNewItem);
 }
 
