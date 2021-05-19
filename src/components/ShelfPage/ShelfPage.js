c9c6be48ecbf82680b029e5b66829b47856a4b57
SamMoudry <sammoudry@gmail.com>
2021-05-19 16:11:22 -0500

    fixed infinite render

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -4,18 +4,20 @@ import {useSelector, useDispatch} from 'react-redux';
 function ShelfPage() {
   const dispatch = useDispatch();
   const list = useSelector(store => store.listReducer.data);
+
+  //Gets the list on page load
   useEffect(() => {
     dispatch({type: 'GET_ITEMS'});
-  }, [list]);
+  }, []);
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      <ul>
+      {list ? <ul>
         {list.map((item) => 
           <li key={item.id}>{item.description}</li>  
         )}
-      </ul>
+      </ul> : ''}
     </div>
   );
 }
