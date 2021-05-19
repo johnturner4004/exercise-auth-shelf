d9cfea8a4ab7fa2df0554fdddcce749edf6a2954
johnturner4004 <johnturner4004@gmail.com>
2021-05-19 16:49:30 -0400

    Create delete router

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -28,6 +28,15 @@ router.post('/', (req, res) => {
  */
 router.delete('/:id', (req, res) => {
   // endpoint functionality
+  let queryText = `DELETE FROM "item" WHERE id=$1;`;
+  pool.query(queryText, [req.params.id])
+  .then((result) => {
+    res.sendStatus(200);
+  })
+  .catch((error => {
+    console.log('Unable to delete item:', error);
+    res.sendStatus(500);
+  }))
 });
 
 /**
