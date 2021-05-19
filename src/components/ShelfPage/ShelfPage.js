e214ae66efba67411858d8e201ef10cd960eda32
SamMoudry <sammoudry@gmail.com>
2021-05-19 15:25:30 -0500

    render list

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 function ShelfPage() {
+  const list = useSelector(store => store.listReducer);
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <ul>
+        {list.map((item) => 
+          <li key={item.id}>{item.description}</li>  
+        )}
+      </ul>
     </div>
   );
 }
