1d398ac89b222a1f2253ea6c03f9f2c6b55a549f
johnturner4004 <johnturner4004@gmail.com>
2021-05-20 14:54:40 -0400

    add delete saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -4,6 +4,7 @@ import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import fetchSaga from './list.saga';
 import addItemSaga from './addItem.saga';
+import deleteItemSaga from './delete.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -19,5 +20,6 @@ export default function* rootSaga() {
     userSaga(),
     fetchSaga(),
     addItemSaga(),
+    deleteItemSaga(),
   ]);
 }
